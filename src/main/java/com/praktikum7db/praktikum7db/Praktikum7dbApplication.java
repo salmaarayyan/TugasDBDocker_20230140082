7d50d01eff4658a7ca9c4f389ed2b80d05445a61
salmaarayyan <salmaarayyan@gmail.com>
2026-04-20 10:04:45 +0700

    Membuat folder project

---
+++ src/main/java/com/praktikum7db/praktikum7db/Praktikum7dbApplication.java
@@ -0,0 +1,13 @@
+package com.praktikum7db.praktikum7db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Praktikum7dbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Praktikum7dbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/praktikum7db/praktikum7db/Praktikum7dbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praktikum7db.praktikum7db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praktikum7dbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
