7d1277f6640aa3619c036a6fb59177ad827d833f
salmaarayyan <salmaarayyan@gmail.com>
2026-04-20 10:05:34 +0700

    Membuat entity model User untuk tabel users

---
+++ src/main/java/com/praktikum7db/praktikum7db/model/User.java
@@ -0,0 +1,25 @@
+package com.praktikum7db.praktikum7db.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "20230140082_users")
+@Builder
+@AllArgsConstructor
+public class User {
+    @Id
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String nim;
+}
